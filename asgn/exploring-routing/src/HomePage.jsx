a2ee3daa3dd74eef86ecc9581316dfe1e4989a17
Aas MOhammad <aasmohd7071@gmail.com>
2026-05-04 16:52:57 +0530

    routing in react done

--- asgn/exploring-routing/src/HomePage.jsx
+++ asgn/exploring-routing/src/HomePage.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
   let [posts, setPosts] = useState([]);
   let url = "https://dummyjson.com/posts";
+  let navigate = useNavigate();
 
   let fetchPost = async () => {
     let res = await axios.get(url);
@@ -11,15 +13,41 @@ export default function HomePage() {
   };
 
   useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
     let fetchData = async () => {
       await fetchPost();
     };
     fetchData();
+
+    return () => {
+      controller.abort();
+    };
   }, []);
+
+  let handlePostClick = (id) => {
+    navigate(`/post/${id}`);
+  };
 
   return (
     <div style={{ marginTop: "40px" }}>
       <h1>Home Page</h1>
+      <div>
+        {posts.map((post, idx) => (
+          <div
+            key={idx}
+            onClick={() => handlePostClick(post.id)}
+            style={{ border: "1px solid black", padding: "10px", margin: "10px", cursor: "pointer" }}
+          >
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+            <span>
+              Likes : {post.reactions.likes} &nbsp; View : {post.views}
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- asgn/exploring-routing/src/PostPage.jsx
+++ asgn/exploring-routing/src/PostPage.jsx
@@ -1,7 +1,48 @@
-export default function PostPage(){
-    return (
-        <div style={{marginTop : "40px"}}>
-            <h1>Post Page</h1>
-       </div>
-    )
+import axios from "axios";
+import { use, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+export default function PostPage() {
+  let { id } = useParams();
+  let url = `https://dummyjson.com/posts/${id}`;
+  let [post, setPost] = useState({ loading: false, data: null, error: null });
+
+  let fetchPost = async () => {
+    setPost({ loading: true, data: null, error: null });
+    try {
+      let res = await axios.get(url);
+      setPost({ loading: false, data: res.data, error: null });
+    } catch (error) {
+      setPost({ loading: false, data: null, error: error.message });
+    }
+  };
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    let fetchData = async () => {
+      await fetchPost();
+    };
+    fetchData();
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  if (post.loading) return <h1>Loading...</h1>;
+  if (post.error) return <h1>Error: {post.error}</h1>;
+
+  return (
+    <div style={{ marginTop: "40px" }}>
+      <h1>Post Page</h1>
+
+      <h3>{post.data?.title}</h3>
+      <p>{post.data?.body}</p>
+      <span>
+        Likes : {post.data?.reactions.likes} &nbsp; View : {post.data?.views}
+      </span>
+      <span>&nbsp; UserId : {post.data?.userId}</span>
+    </div>
+  );
 }
