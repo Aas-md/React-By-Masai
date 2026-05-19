6cd2cc7b124a5e160b4078521c3590b0d86cc199
Aas MOhammad <aasmohd7071@gmail.com>
2026-05-19 20:10:21 +0530

    Working on context api

--- asgn/context-api-2/src/App.jsx
+++ asgn/context-api-2/src/App.jsx
@@ -3,21 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-import Home from "./Home";
+import Home from "./q-2/Home";
 
-import { AuthProvider } from "./DataContext";
 function App() {
   return (
     <>
-    <AuthProvider>
-      <Navbar/>
-      <Home/>
-      <Footer/>
-    </AuthProvider>
-  </>
-  )
+     <Home/>
+    </>
+  );
 }
 
 export default App;
